532df3ea8c820c16e059cb2901b8e5216857d3fc
FUGAMARU <gofugashi@gmail.com>
2022-12-31 03:26:47 +0900

    [Update] 矢印キーの連続入力に対応

--- MacHomeEnd/MyKeyboardHook.cs
+++ MacHomeEnd/MyKeyboardHook.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Runtime.InteropServices;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using System.Windows.Input;
-using WindowsInput.Native;
 using WindowsInput;
 
 namespace MacHomeEnd
@@ -12,7 +7,9 @@ namespace MacHomeEnd
     class MyKeyboardHook : KeyboardHook
     {
         private InputSimulator inputSimulator = new InputSimulator();
-        private List<short> pressingKeys = new List<short>();
+
+        private int currentParam = 0;
+        private short pressedPhysicalKey = 0;
 
         public void Subscribe() => Hook();
         public void UnSubscribe() => UnHook();
@@ -21,53 +18,41 @@ namespace MacHomeEnd
         {
             if (nCode < 0) return new IntPtr(0);
 
+            if (currentParam == 0) currentParam = (int)lParam;
+
+            var keyCode = (short)((KBDLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(KBDLLHOOKSTRUCT))).vkCode;
+
             // キーダウン
             if (wParam == (IntPtr)WM_KEYDOWN || wParam == (IntPtr)WM_SYSKEYDOWN)
             {
-                var keyCode = getKeyCode(lParam);
-                //Console.WriteLine($"{keyCode}, onKeyDown");
-                pressingKeys.Add(keyCode);
+                // 物理的にCtrlキーが押された場合キーコードを更新
+                if ((keyCode == 162 || keyCode == 163) && (currentParam == (int)lParam)) pressedPhysicalKey = keyCode;
 
-                if ((keyCode == 37 || keyCode == 39) && (pressingKeys.Contains(162) || pressingKeys.Contains(163)))
+                var isLeftCtrlDown = inputSimulator.InputDeviceState.IsKeyDown(VirtualKeyCode.LCONTROL);
+                var isRightCtrlDown = inputSimulator.InputDeviceState.IsKeyDown(VirtualKeyCode.RCONTROL);
+
+                if ((keyCode == 37 || keyCode == 39) && (isLeftCtrlDown || isRightCtrlDown))
                 {
-                    switch (keyCode)
-                    {
-                        case 37:
-                            Console.WriteLine("Home");
-                            releaseCtrl();
-                            inputSimulator.Keyboard.KeyPress(VirtualKeyCode.HOME);
-                            return new IntPtr(1);
-                        case 39:
-                            Console.WriteLine("End");
-                            releaseCtrl();
-                            inputSimulator.Keyboard.KeyPress(VirtualKeyCode.END);
-                            return new IntPtr(1);
-                    }
+                    releaseCtrl();
+                    inputSimulator.Keyboard.KeyPress(keyCode == 37 ? VirtualKeyCode.HOME : VirtualKeyCode.END);
+                    pressCtrl();
+                    return new IntPtr(1);
                 }
-            }
-
-            // キーアップ
-            if (wParam == (IntPtr)WM_KEYUP || wParam == (IntPtr)WM_SYSKEYUP)
-            {
-                //Console.WriteLine($"{getKeyCode(lParam)}, onKeyUp");
-                pressingKeys.Remove(getKeyCode(lParam));
             }
 
             return new IntPtr(0);
         }
 
-        private short getKeyCode(IntPtr lParam)
-        {
-            var kb = (KBDLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(KBDLLHOOKSTRUCT));
-            var vkCode = (short)kb.vkCode;
-            return vkCode;
-        }
-
         private void releaseCtrl()
         {
             inputSimulator.Keyboard.KeyUp(VirtualKeyCode.LCONTROL);
-            pressingKeys.Remove(162);
-            pressingKeys.Remove(163);
+            inputSimulator.Keyboard.KeyUp(VirtualKeyCode.RCONTROL);
+        }
+
+        private void pressCtrl()
+        {
+            if (pressedPhysicalKey == 162) inputSimulator.Keyboard.KeyDown(VirtualKeyCode.LCONTROL);
+            if (pressedPhysicalKey == 163) inputSimulator.Keyboard.KeyDown(VirtualKeyCode.RCONTROL);
         }
     }
 }
