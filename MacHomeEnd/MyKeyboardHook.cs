fbaa04e5c399f3bff5a6c52f3dc1e60235ce2cdc
FUGAMARU <gofugashi@gmail.com>
2022-12-30 21:09:21 +0900

    [Add] HomeキーとEndキーの入力タイミング判定用ロジックを実装

--- MacHomeEnd/MyKeyboardHook.cs
+++ MacHomeEnd/MyKeyboardHook.cs
@@ -1,32 +1,64 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
 
 namespace MacHomeEnd
 {
     class MyKeyboardHook : KeyboardHook
     {
+        private List<int> pressingKeys = new List<int>();
+
         public void Subscribe() => Hook();
         public void UnSubscribe() => UnHook();
 
         public override IntPtr HookProcedure(int nCode, IntPtr wParam, IntPtr lParam)
         {
-            if (nCode >= 0 && (wParam == (IntPtr)WM_KEYDOWN || wParam == (IntPtr)WM_SYSKEYDOWN))
+            if (nCode < 0) return new IntPtr(0);
+
+            // キーダウン
+            if (wParam == (IntPtr)WM_KEYDOWN || wParam == (IntPtr)WM_SYSKEYDOWN)
             {
-                Console.WriteLine($"{GetKeyCode(lParam)}, onKeyDown");
+                var keyCode = getKeyCode(lParam);
+                //Console.WriteLine($"{keyCode}, onKeyDown");
+                pressingKeys.Add(keyCode);
+
+                if ((keyCode == 37 || keyCode == 39) && (pressingKeys.Contains(162) || pressingKeys.Contains(163)))
+                {
+                    switch (keyCode)
+                    {
+                        case 37:
+                            Console.WriteLine("Home");
+                            releaseCtrl();
+                            return new IntPtr(1);
+                        case 39:
+                            Console.WriteLine("End");
+                            releaseCtrl();
+                            return new IntPtr(1);
+                    }
+                }
             }
-            else if (nCode >= 0 && (wParam == (IntPtr)WM_KEYUP || wParam == (IntPtr)WM_SYSKEYUP))
+
+            // キーアップ
+            if (wParam == (IntPtr)WM_KEYUP || wParam == (IntPtr)WM_SYSKEYUP)
             {
-                Console.WriteLine($"{GetKeyCode(lParam)}, onKeyUp");
+                //Console.WriteLine($"{getKeyCode(lParam)}, onKeyUp");
+                pressingKeys.Remove(getKeyCode(lParam));
             }
 
             return new IntPtr(0);
         }
 
-        private int GetKeyCode(IntPtr lParam)
+        private int getKeyCode(IntPtr lParam)
         {
             var kb = (KBDLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(KBDLLHOOKSTRUCT));
             var vkCode = (int)kb.vkCode;
             return vkCode;
+        }
+
+        private void releaseCtrl()
+        {
+            pressingKeys.Remove(162);
+            pressingKeys.Remove(163);
         }
     }
 }
