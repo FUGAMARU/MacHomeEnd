8709cfc30c51f41cce46c7dc1a4159f0aa87960c
FUGAMARU <gofugashi@gmail.com>
2023-01-03 19:01:01 +0900

    [Add] コンテキストメニューに設定項目を追加

--- MacHomeEnd/SettingControl.cs
+++ MacHomeEnd/SettingControl.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Windows.Forms;
 
 namespace MacHomeEnd
 {
@@ -7,16 +8,16 @@ namespace MacHomeEnd
         public static ContextMenuStrip SetContextMenu()
         {
             ContextMenuStrip menu = new ContextMenuStrip();
-            ToolStripMenuItem menuItem = new ToolStripMenuItem();
 
-            var exit = new ToolStripMenuItem("Exit", null, (s1, e1) =>
-            {
-                System.Windows.Application.Current.Shutdown();
-            });
+            var isEnableCmdKey = new ToolStripMenuItem("command", null, (s, e) => Console.WriteLine("commandを有効化する"));
+            isEnableCmdKey.Checked = true;
 
-            menu.Items.AddRange(new ToolStripMenuItem[]{
-                exit
-            });
+            var isEnableOptionKey = new ToolStripMenuItem("option", null, (s, e) => Console.WriteLine("optionを有効化する"));
+            isEnableOptionKey.Checked = true;
+
+            var exit = new ToolStripMenuItem("Exit", null, (s, e) => System.Windows.Application.Current.Shutdown());
+
+            menu.Items.AddRange(new ToolStripItem[] { isEnableCmdKey, isEnableOptionKey, new ToolStripSeparator(), exit });
 
             return menu;
         }
